ec0b3fc64d5aad118e33afe6fcc66347cc433fe1
Jansiya jahan <jansiyajahan8@gmail.com>
2024-06-10 12:41:44 +0530

    setting Up the middle Ware for handleing errors

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const port=process.env.PORT||8000
+ import userRoutes from './routes/userRoutes.js'
+
  const app= express();
+
+ app.use('/api/users',userRoutes)
 
  app.get ('/',(req,res)=> res.send('server is running'))
  app.listen(port,()=>console.log(`server started ${port}`))
